fa3a0f4891eabc46c09bf40b47ab55759d9126b8
Mahtab Alam <smmalam9@gmail.com>
2025-01-02 21:13:33 +0530

    Match Alphanumeric characters

--- src/Server.cpp
+++ src/Server.cpp
@@ -9,6 +9,10 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     {
 		return input_line.find_first_of("0123456789") != std::string::npos; 
     }
+    else if (pattern == "\\w")
+    {
+        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != std::string::npos; 
+    }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
