126340199497149a748b4e2b3a0c48c61d1ba99d
Mahtab Alam <smmalam9@gmail.com>
2025-01-03 22:09:09 +0530

    start_of_string_anchor

--- src/Server.cpp
+++ src/Server.cpp
@@ -79,8 +79,6 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
 
 bool match_patterns(std::string& input_line, std::string& pattern)
 {
-    if (pattern.empty()) return true;
-
     if (pattern[0] == '^')
     {
         // Ensure the match starts from the beginning of the input_line
@@ -98,5 +96,50 @@ bool match_patterns(std::string& input_line, std::string& pattern)
         } while (!input_line.empty());
 
         return false;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Flush after every std::cout / std::cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    std::cerr << "Logs from your program will appear here" << std::endl;
+
+    if (argc != 3)
+    {
+        std::cerr << "Expected two arguments" << std::endl;
+        return 1;
+    }
+
+    std::string flag = argv[1];
+    std::string pattern = argv[2];
+
+    if (flag != "-E")
+    {
+        std::cerr << "Expected first argument to be '-E'" << std::endl;
+        return 1;
+    }
+
+    std::string input_line;
+    std::getline(std::cin, input_line);
+
+    try
+    {
+        if (match_patterns(input_line, pattern))
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
 }
