475b618ca56c712e49b0de5d74c94ac6115e9c4d
Mahtab Alam <smmalam9@gmail.com>
2025-01-03 20:52:12 +0530

    start_of_string_anchor

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <string>
 
-bool match_digits(const std::string& inputs, const std::string& pattern) {
-    for (auto input : inputs) {
-        if (isdigit(input)) {
+bool match_digits(const std::string& inputs, const std::string& pattern) 
+{
+    for (auto input : inputs) 
+    {
+        if (isdigit(input)) 
+        {
             return true;
         }
     }
     return false;
 }
 
-bool match_alphanumeric(const std::string& inputs, const std::string& pattern) {
-    for (auto input : inputs) {
-        if (isalnum(input)) {
+bool match_alphanumeric(const std::string& inputs, const std::string& pattern) 
+{
+    for (auto input : inputs) 
+    {
+        if (isalnum(input)) 
+        {
             return true;
         }
     }
     return false;
 }
 
-bool match_group(const std::string& inputs, const std::string& pattern) {
+bool match_group(const std::string& inputs, const std::string& pattern) 
+{
     return inputs.find_first_of(pattern) != std::string::npos;
 }
 
@@ -27,33 +34,47 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.size() == 0) return true;
     if (input_line.size() == 0) return false;
 
-    if (pattern.substr(0, 2) == "\\d") {
-        if (isdigit(input_line[0])) {
+    if (pattern.substr(0, 2) == "\\d") 
+    {
+        if (isdigit(input_line[0])) 
+        {
             return match_pattern(input_line.substr(1), pattern.substr(2));
         }
         return match_pattern(input_line.substr(1), pattern);
     }
-    else if (pattern.substr(0, 2) == "\\w") {
-        if (isalnum(input_line[0])) {
+    else if (pattern.substr(0, 2) == "\\w") 
+    {
+        if (isalnum(input_line[0])) 
+        {
             return match_pattern(input_line.substr(1), pattern.substr(2));
         }
         return match_pattern(input_line.substr(1), pattern);
     }
-    else if (pattern[0] == '[') {
+    else if (pattern[0] == '[') 
+    {
         auto pos = pattern.find_first_of("]");
-        if (pattern[1] == '^') {
-            if (!match_group(input_line, pattern.substr(2, pos - 2))) {
+        if (pattern[1] == '^') 
+        {
+            if (!match_group(input_line, pattern.substr(2, pos - 2))) 
+            {
                 return match_pattern(input_line.substr(1), pattern.substr(pos + 1));
             }
             return false;
         }
-        if (match_group(input_line, pattern.substr(1, pos - 1))) {
+        if (match_group(input_line, pattern.substr(1, pos - 1))) 
+        {
             return match_pattern(input_line.substr(1), pattern.substr(pos + 1));
         }
         return false;
     }
 
-    if (pattern[0] == input_line[0]) {
+    else if (pattern[0] == '^')
+    {
+        return match_pattern(input_line, pattern.substr(1));
+    }
+
+    if (pattern[0] == input_line[0]) 
+    {
         return match_pattern(input_line.substr(1), pattern.substr(1));
     }
 
