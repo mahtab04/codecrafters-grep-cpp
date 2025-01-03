7a26eba8710f1ae191e3aaf73707126a7cb5c63c
Mahtab Alam <smmalam9@gmail.com>
2025-01-03 22:03:50 +0530

    start_of_string_anchor

--- src/Server.cpp
+++ src/Server.cpp
@@ -30,6 +30,7 @@ bool match_group(const std::string& inputs, const std::string& pattern)
     return inputs.find_first_of(pattern) != std::string::npos;
 }
 
+
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.size() == 0) return true;
     if (input_line.size() == 0) return false;
@@ -68,11 +69,6 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
         return false;
     }
 
-    else if (pattern[0] == '^')
-    {
-        return match_pattern(input_line, pattern.substr(1));
-    }
-
     if (pattern[0] == input_line[0]) 
     {
         return match_pattern(input_line.substr(1), pattern.substr(1));
@@ -81,59 +77,27 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     return match_pattern(input_line.substr(1), pattern);
 }
 
-bool match_patterns(std::string& input_line, std::string& pattern) 
+bool match_patterns(std::string& input_line, std::string& pattern)
 {
-    do {
-        if (match_pattern(input_line, pattern)) {
-            return true;
-        }
-        input_line = input_line.substr(1);
-    } while (!input_line.empty());
+    if (pattern.empty()) return true;
 
-    return false;
-}
-
-int main(int argc, char *argv[])
-{
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
-
-    if (argc != 3)
+    if (pattern[0] == '^')
     {
-        std::cerr << "Expected two arguments" << std::endl;
-        return 1;
+        // Ensure the match starts from the beginning of the input_line
+        return match_pattern(input_line, pattern.substr(1));
     }
-
-    std::string flag = argv[1];
-    std::string pattern = argv[2];
-
-    if (flag != "-E")
+    else
     {
-        std::cerr << "Expected first argument to be '-E'" << std::endl;
-        return 1;
-    }
-
-    std::string input_line;
-    std::getline(std::cin, input_line);
-
-    try
-    {
-        if (match_patterns(input_line, pattern))
+        do
         {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
-    }
-    catch (const std::runtime_error &e)
-    {
-        std::cerr << e.what() << std::endl;
-        return 1;
+            if (match_pattern(input_line, pattern))
+            {
+                return true;
+            }
+            input_line = input_line.substr(1); // Move to the next character
+        } while (!input_line.empty());
+
+        return false;
     }
 }
+8
