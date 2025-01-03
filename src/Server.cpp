941bec6cf352dabd2c69b7123733cb78468cd624
Mahtab Alam <smmalam9@gmail.com>
2025-01-03 20:33:37 +0530

    Combining Character

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,35 +1,75 @@
 #include <iostream>
 #include <string>
 
-bool match_pattern(const std::string &input_line, const std::string &pattern)
-{
-    if (pattern.length() == 1)
-    {
-        return input_line.find(pattern) != std::string::npos;
-    }
-    else if (pattern == "\\d")
-    {
-        return input_line.find_first_of("0123456789") != std::string::npos;
-    }
-    else if (pattern == "\\w")
-    {
-        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != std::string::npos;
-    }
-    else if (pattern.starts_with("[") && pattern.ends_with("]"))
-    {
-        if (pattern[1] == '^')
-        {
-            std::string chars = pattern.substr(2, pattern.length() - 3);
-            return input_line.find_first_not_of(chars) != std::string::npos;
+bool match_digits(const std::string& inputs, const std::string& pattern) {
+    for (auto input : inputs) {
+        if (isdigit(input)) {
+            return true;
         }
-        std::string chars = pattern.substr(1, pattern.length() - 2);
-        return input_line.find_first_of(chars) != std::string::npos;
+    }
+    return false;
+}
+
+bool match_alphanumeric(const std::string& inputs, const std::string& pattern) {
+    for (auto input : inputs) {
+        if (isalnum(input)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool match_group(const std::string& inputs, const std::string& pattern) {
+    return inputs.find_first_of(pattern) != std::string::npos;
+}
+
+bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    if (pattern.size() == 0) return true;
+    if (input_line.size() == 0) return false;
+
+    if (pattern.substr(0, 2) == "\\d") {
+        if (isdigit(input_line[0])) {
+            return match_pattern(input_line.substr(1), pattern.substr(2));
+        }
+        return match_pattern(input_line.substr(1), pattern);
+    }
+    else if (pattern.substr(0, 2) == "\\w") {
+        if (isalnum(input_line[0])) {
+            return match_pattern(input_line.substr(1), pattern.substr(2));
+        }
+        return match_pattern(input_line.substr(1), pattern);
+    }
+    else if (pattern[0] == '[') {
+        auto pos = pattern.find_first_of("]");
+        if (pattern[1] == '^') {
+            if (!match_group(input_line, pattern.substr(2, pos - 2))) {
+                return match_pattern(input_line.substr(1), pattern.substr(pos + 1));
+            }
+            return false;
+        }
+        if (match_group(input_line, pattern.substr(1, pos - 1))) {
+            return match_pattern(input_line.substr(1), pattern.substr(pos + 1));
+        }
+        return false;
     }
 
-    else
-    {
-        throw std::runtime_error("Unhandled pattern " + pattern);
+    if (pattern[0] == input_line[0]) {
+        return match_pattern(input_line.substr(1), pattern.substr(1));
     }
+
+    return match_pattern(input_line.substr(1), pattern);
+}
+
+bool match_patterns(std::string& input_line, std::string& pattern) 
+{
+    do {
+        if (match_pattern(input_line, pattern)) {
+            return true;
+        }
+        input_line = input_line.substr(1);
+    } while (!input_line.empty());
+
+    return false;
 }
 
 int main(int argc, char *argv[])
@@ -61,7 +101,7 @@ int main(int argc, char *argv[])
 
     try
     {
-        if (match_pattern(input_line, pattern))
+        if (match_patterns(input_line, pattern))
         {
             return 0;
         }
